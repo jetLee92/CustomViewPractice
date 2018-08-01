0680d6c9607075730fc8b4ab59994d332adfcddd
jetLee92 <940077208@qq.com>
2018-08-01 18:29:32 +0800

    三维效果初写

--- app/src/main/java/com/jetLee/customviewpractice/ui/CanvasMatrixCamera/ClipActivity.java
+++ app/src/main/java/com/jetLee/customviewpractice/ui/CanvasMatrixCamera/ClipActivity.java
@@ -1,10 +1,13 @@
 package com.jetLee.customviewpractice.ui.CanvasMatrixCamera;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.jetLee.customviewpractice.R;
+import com.jetLee.customviewpractice.view.CanvasMatrixCamera.ClipView;
 
 /**
  * @author：Jet啟思
@@ -12,9 +15,26 @@ import com.jetLee.customviewpractice.R;
  */
 public class ClipActivity extends AppCompatActivity {
 
+	ClipView clipView;
+
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_clip);
+
+		clipView = findViewById(R.id.clip);
+
+		ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(clipView,"rightAngle",-15);
+		objectAnimator.setDuration(2000);
+
+		ObjectAnimator objectAnimator2 = ObjectAnimator.ofFloat(clipView, "rotation", 270);
+		objectAnimator2.setStartDelay(300);
+		objectAnimator2.setDuration(2000);
+
+		AnimatorSet animatorSet = new AnimatorSet();
+		animatorSet.playSequentially(objectAnimator2);
+		animatorSet.setStartDelay(500);
+		animatorSet.start();
+
 	}
 }
--- app/src/main/java/com/jetLee/customviewpractice/view/CanvasMatrixCamera/ClipView.java
+++ app/src/main/java/com/jetLee/customviewpractice/view/CanvasMatrixCamera/ClipView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Camera;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
@@ -20,67 +21,84 @@ import com.jetLee.customviewpractice.Utils;
  */
 public class ClipView extends View {
 
-	Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-	private Bitmap bitmap;
-	private int imageSize = Utils.dpToPx(200);
-	private int offset = Utils.dpToPx(100);
-	private int PADDING = Utils.dpToPx(20);
-	private int corner = Utils.dpToPx(20);
-	private RectF rectF = new RectF();
-	private RectF rectP = new RectF();
-	private Camera camera;
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Bitmap bitmap;
+    private int imageSize = Utils.dpToPx(220);
+    private int offset = Utils.dpToPx(100);
+    private int PADDING = Utils.dpToPx(50);
+    private int corner = Utils.dpToPx(20);
+    private RectF rectF = new RectF();
+    private RectF rectP = new RectF();
+    private Camera camera;
 
-	public ClipView(Context context) {
-		super(context);
-	}
+    private float rightAngle;
+    private float rotation;
 
-	public ClipView(Context context, @Nullable AttributeSet attrs) {
-		super(context, attrs);
-//		this.setLayerType(View.LAYER_TYPE_NONE, paint);
-		init();
-	}
+    public ClipView(Context context) {
+        super(context);
+    }
 
-	private void init() {
-		bitmap = Utils.getBitmap(getResources(), imageSize);
-		camera = new Camera();
-	}
+    public ClipView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-//		canvas.rotate(30);
-//		canvas.translate(offset, 0);
-//		canvas.translate(-offset, 0);
+    private void init() {
+        bitmap = Utils.getBitmap(getResources(), imageSize);
+        camera = new Camera();
+    }
 
-//		Matrix matrix = new Matrix();
-//		matrix.reset();
-//		matrix.postRotate(30);
-//		matrix.postTranslate(0, offset);
-//		matrix.setTranslate(offset,0); // set会清空matrix的所有变换
-//		matrix.preTranslate(offset,0); // 矩阵前乘
-//		canvas.concat(matrix);
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
-		canvas.save();
-		camera.save();
-//		camera.translate(offset, 0, 0);
-//		camera.rotateX(-30);
-//		canvas.translate(imageSize / 2 + PADDING, imageSize / 2 + PADDING);
-		camera.applyToCanvas(canvas);
-//		canvas.translate(-(imageSize / 2 + PADDING), -(imageSize / 2 + PADDING));
-		camera.restore();
+        canvas.save();
 
-		rectF.set(0, 0, getWidth(), getHeight());
-		int saved = canvas.saveLayer(rectF, paint);
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+        canvas.clipRect(-imageSize, -imageSize, 0, imageSize);
+        canvas.translate(-getWidth() / 2, -getHeight() / 2);
 
-//		 画圆角图片
-		rectP.set(PADDING, PADDING, imageSize + PADDING, imageSize + PADDING);
-		canvas.drawRoundRect(rectP, corner, corner, paint);
+        canvas.drawBitmap(bitmap, (getWidth() - imageSize) / 2, (getHeight() - imageSize) / 2, paint);
 
-		paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-		canvas.drawBitmap(bitmap, PADDING, PADDING, paint);
-		paint.setXfermode(null);
+        canvas.restore();
 
-		canvas.restoreToCount(saved);
-		canvas.restore();
-	}
+
+        canvas.save();
+        camera.save();
+//        camera.rotateY(rightAngle);
+
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+        canvas.rotate(rotation);
+//        camera.applyToCanvas(canvas);
+        canvas.rotate(-rotation);
+        canvas.clipRect(0, -imageSize, imageSize, imageSize);
+        canvas.translate(-getWidth() / 2, -getHeight() / 2);
+        canvas.drawBitmap(bitmap, (getWidth() - imageSize) / 2, (getHeight() - imageSize) / 2, paint);
+
+        camera.restore();
+        canvas.restore();
+
+    }
+
+    public float getRightAngle() {
+        return rightAngle;
+    }
+
+    public void setRightAngle(float rightAngle) {
+        this.rightAngle = rightAngle;
+        invalidate();
+    }
+
+    @Override
+    public float getRotation() {
+        return rotation;
+    }
+
+    @Override
+    public void setRotation(float rotation) {
+        this.rotation = rotation;
+        invalidate();
+    }
+
+
 }
